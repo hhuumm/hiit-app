31c218594211c99569d5d952ca34d567e4052776
hhuumm <h4mid@Hotmail.com>
2023-03-10 16:03:57 -0500

    Start and rest functionality

--- src/App.js
+++ src/App.js
@@ -12,31 +12,61 @@ import AudioPlayer from 'react-audio-player';
 
 function App() {
   const [time, setTime] = useState(60);
+  
   const [isRunning, setIsRunning] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [workoutState, setWorkoutState] = useState('rest');
+  const [workoutColor, setWorkoutColor] = useState('green');
   const intervalRef = useRef(null);
   const rap = useRef();
+  const title = useRef();
+  let round = 1;
+  let chill = 240;
+  let intense = 60;
 
   function toggleWorkout() {
-    console.log(rap.current.audioEl)
     if (isRunning) {
       stopWorkout();
     } else {
-
+      title.current.hidden = false;
       startWorkout();
     }
 
   }
 
+  function rest() {
+    setWorkoutState("Rest");
+    setWorkoutColor("grey");
+    setTime(chill);
+    round++;
+    clearInterval(intervalRef.current);
+    console.log(round);
+    intervalRef.current = setInterval(() => {
+      setTime((prevTime) => {
+        if (prevTime <= 1 && round <= 3) {
+          startWorkout();
+          return 0;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+
+  }
   
   function startWorkout() {
+    clearInterval(intervalRef.current);
     setIsRunning(true);
-
+    setWorkoutState("Intense!");
+    setWorkoutColor("Green");
+    setTime(intense);
     //play the audio when the timer starts
     intervalRef.current = setInterval(() => {
       setTime((prevTime) => {
-        if (prevTime <= 1) {
+        if (round === 3) {
           stopWorkout();
+          return 0;
+        } else if(prevTime <= 1 && round < 3)  {
+          rest();
           return 0;
         }
         return prevTime - 1;
@@ -58,13 +88,14 @@ function App() {
 
  
     <div className="App">
-    <h1>{time}</h1>
-    <AudioPlayer
+      <h2 ref = {title} style={{color:workoutColor}} hidden ={isRunning?false:true}>{workoutState}</h2>
+    <h1 hidden ={isRunning?false:true}>{time}</h1>
+    {/* <AudioPlayer
       ref={rap}
       src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
       controls
       autoPlay={isRunning}
-    />
+    /> */}
     <Button
       variant={isRunning ? 'outlined' : 'contained'}
       color={isRunning ? 'error' : 'primary'}
